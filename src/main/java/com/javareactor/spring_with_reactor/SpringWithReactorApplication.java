a9093c66113097f82cc5cbf9be2d9cfbf1ff23de
benji_mac <benaja.bendo02@gmail.com>
2024-02-16 14:01:03 +0100

    premier commit

---
+++ src/main/java/com/javareactor/spring_with_reactor/SpringWithReactorApplication.java
@@ -0,0 +1,13 @@
+package com.javareactor.spring_with_reactor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithReactorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWithReactorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/javareactor/spring_with_reactor/SpringWithReactorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javareactor.spring_with_reactor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWithReactorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
